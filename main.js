b8788ee0ac8d7281d4e5a2ab8d0f4b8537b63b37
Leemon <leemon174@gmail.com>
2022-06-20 04:48:59 +0600

    second Commit

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ let weather = {
         document.querySelector('.city').innerText = 'Weather in ' + name;
         document.querySelector('.icon').src =  "https://openweathermap.org/img/wn/"+icon+".png";
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".temp").innerText = Math.round(temp) + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed " + speed + "km/h";
         document.querySelector(".weather").classList.remove("loading");
